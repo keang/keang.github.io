70fa8dfa831f7466f18fecebdf1fcb59b515060b
Keang <skeang@gmail.com>
2014-03-21 04:16:27 +0800

    todo

--- js/luna.js
+++ js/luna.js
@@ -144,5 +144,5 @@ function HomeCtrl($scope, $http){
     console.log('all projects loaded');
   });
 
-  $scope.currentTask = ["Finish thesis and graduate."];
+  $scope.currentTask = ["Finish thesis and graduate.", "Part-time at Gimmieworld"];
 }
